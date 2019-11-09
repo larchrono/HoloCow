33a8b16c92c85006ff639f378e4c0a6350347e7d
larchrono <32762325+larchrono@users.noreply.github.com>
2019-11-10 00:33:35 +0800

    Upload Coin Arduino
    這個專案使用到3個 COM Port , 兩個Arduino Code

---
+++ CoinModule/CoinModule.ino
@@ -0,0 +1,26 @@
+int coinnum = 0 ;
+const int ledPin = 13;                   // LED
+const int buttonPin = 2;                 // 按鈕(pushbutton)
+
+void setup() {
+  Serial.begin (9600);             // Serial Port begin
+  pinMode(ledPin, OUTPUT); 
+  pinMode(buttonPin, INPUT);
+  
+  digitalWrite(ledPin, LOW);
+  
+  attachInterrupt(0, addcoin, RISING ); 
+}
+
+void loop() { 
+
+}
+
+void addcoin() {
+ coinnum ++ ;
+ Serial.println(coinnum);
+ 
+ digitalWrite(ledPin, HIGH);
+ delay(500);
+ digitalWrite(ledPin, LOW);
+}
